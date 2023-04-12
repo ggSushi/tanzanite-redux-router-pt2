b8942703012dab8380e1833b112f0a2ad9185bd6
Glonel Dimapilis <gdimapilis@aol.com>
2023-04-12 18:27:26 -0500

    properly set up Step Four. Working on Review page

---
+++ src/components/Steps/StepFour.jsx
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+
+function StepFour() {
+  const dispatch = useDispatch();
+  const miles = useSelector(store => store.miles);
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    const action = {
+      type: 'SET_ACTIVITY_MILES',
+      payload: event.target.value
+    };
+    dispatch(action);
+  }
+
+  const nextPage = (event) => {
+    event.preventDefault();
+    if (miles > 0) {
+      history.push('/review');
+    } else {
+      alert(`Please enter a value greater than 0.`);
+    }
+  }
+
+  return (
+    <>
+      <h3>Step Four</h3>
+      <form onSubmit={nextPage}>
+        <label htmlFor="miles">Miles:</label>
+        <br />
+        <input type="number" value={miles} onChange={handleChange} />
+        <input type="submit" value="Next" />
+      </form>
+    </>
+  )
+}
+
+export default StepFour;
